ae071411ed7f60fbaaaa4738b521d3db1d371374
josi321 <josi_on83@hotmail.com>
2017-03-09 00:09:06 -0500

    #07: styling
    products-archive styling. still need tweaking

--- themes/blogWP/template-parts/content-product.php
+++ themes/blogWP/template-parts/content-product.php
@@ -4,7 +4,7 @@
 <div class="row">
 
 
- <div class="col-xs-3">
+ <!-- <div class="col-xs-3">
    <?php
    $query_products = new WP_Query(array(
                  "posts_per_page" => 16,
@@ -26,4 +26,44 @@
      <?php endif; ?>
  </div>
 
-</div>
+</div> -->
+
+<?php
+    $a=array(
+    'post_type' => 'products',
+    'posts_per_page' => 16
+    );
+    $my_query = null;
+    $my_query = new WP_Query($a);
+
+if( $my_query->have_posts() ) {
+    echo '';
+$i = 0;
+while ($my_query->have_posts()) : $my_query->the_post();
+    if($i % 4 == 0) { ?>
+        <div class="row">
+            <?php
+            }
+            ?>
+            <div class="col-xs-3">
+              <div class="product-grids">
+                  <?php echo the_post_thumbnail("small"); ?>
+                  <p class="price">
+                    <a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a>.....<?php echo get_field('Price'); ?>
+                 </p>
+              </div>
+            </div>
+
+            <?php $i++;
+     if($i != 0 && $i % 4 == 0) { ?>
+       </div><!--/.row-->
+       <div class="clearfix"></div>
+
+     <?php
+      } ?>
+
+     <?php
+       endwhile;
+       }
+       wp_reset_query();
+       ?>
