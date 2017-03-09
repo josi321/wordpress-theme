2d833f40d44db51fd20f1f95fd132d76d532eaba
josi321 <josi_on83@hotmail.com>
2017-03-09 13:58:39 -0500

    #017: adventure archive
    still needs work on styling

--- themes/blogWP/archive-adventures.php
+++ themes/blogWP/archive-adventures.php
@@ -12,4 +12,32 @@ get_header(); ?>
   <h1>LATEST ADVENTURES</h1>
 </div>
 
+<div class="container-adventure">
+  <div class="adventure-grid">
+  <?php
+      $query_adventure = new WP_Query(array(
+           "posts_per_page" => 4,
+           "post_type" => "adventures",
+           "order" => "ASC"
+           ));
+
+       ?>
+  <?php if(have_posts()): ?>
+    <?php while($query_adventure->have_posts() ) : $query_adventure->the_post(); ?>
+  <div class="col-md-6">
+    <img <?php the_post_thumbnail(); ?>
+    <div class="ad-title-btn">
+      <h1> <?php echo the_title(); ?></h1>
+      <a href="<?php the_permalink(); ?>">
+        <h2>READ MORE</h2>
+      </a>
+    </div>
+  </div>
+<?php endwhile; ?>
+<?php else: ?>
+  <?php wpautop('Sorry, no post were found!') ?>
+<?php endif; ?>
+</div>
+</div>
+
 <?php get_footer(); ?>
