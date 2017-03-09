67e1ad5a62515f7f2a6efa17d3091fc480949141
josi321 <josi_on83@hotmail.com>
2017-03-09 12:40:45 -0500

    #12:journal archive page

--- themes/blogWP/page-journal.php
+++ themes/blogWP/page-journal.php
@@ -1,18 +1,28 @@
 in the the journal page
 <?php get_header(); ?>
-
+<br /><br /><br /><br /><br />
 <div class="container content">
-  		<div class="main">
-        <br><br><br><br><br>
- 			<?php while ( have_posts() ) : the_post(); ?>
-        <?php echo the_post_thumbnail(); ?>
- 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		<div class="main block">
+<?php
+  $args = array(
+    'posts_per_page' => 6,
+    'order'=> 'DESC',
+    'orderby' => 'date' );
+  $postslist = get_posts( $args );
+  foreach ( $postslist as $post ) :
+  setup_postdata( $post ) ; ?>
+  <div class="height-containers">
+    <h2><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+    <img <?php the_post_thumbnail(); ?>
+    <h4><?php the_date('F j, Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></h4>
+    <br><br>
+    <a class="read-btn" href="<?php the_permalink(); ?>">READ ENTRY</a>
+    <br><br><br><br>
+  </div>
 
-
- 			<?php endwhile; // End of the loop. ?>
-
-
+<?php endforeach; ?>
+</div>
+<?php get_sidebar(); ?>
 </div>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
