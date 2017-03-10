0cfb4409e12d265afe617f3eddf94efa34f4740f
josi321 <josi_on83@hotmail.com>
2017-03-10 16:39:57 -0500

    #27: single journal post
    styled single journal post, but in turns of event, it ruined the layout
    for journal archive, so i need to fix that

--- themes/blogWP/single.php
+++ themes/blogWP/single.php
@@ -1,38 +1,38 @@
-single page
+<!-- single page -->
 
 <?php get_header(); ?>
+<br><br><br>
 <div class="container content">
 		<div class="main block">
   			<?php if(have_posts()) : ?>
+
   				<?php while(have_posts()): the_post(); ?>
-            <article class="post">
-                <!-- article is like a div, it wraps around each post -->
+            <article class="journal-single-post">
                 <h3>
                     <?php the_title(); ?>
                 </h3>
-                <div>
-      					Created By <?php the_author(); ?> on <?php the_time('F j, Y g:i a'); ?>
-      					</div>
-
-                <!-- we added the image to the overview page, now we need to add image to single page. check to see if theres an img, if there is then post it-->
-                <?php if(has_post_thumbnail()): ?>
-                    <div class="post-thumbnail">
-                        <?php the_post_thumbnail(); ?>
-                    </div>
-                <?php endif; ?>
-                <br><br><br>
-      					<?php the_content(); ?>
-                <br />
+								<?php if(has_post_thumbnail()): ?>
+										<div class="post-thumbnail">
+												<?php the_post_thumbnail(); ?>
+										</div>
+								<?php endif; ?>
+								<h4><?php the_date('F j, Y'); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / BY <?php the_author(); ?></h4
+								<br />
+									<p>
+										<?php echo the_content(); ?>
+									</p>
             </article>
+
   				<?php endwhile; ?>
   			<?php else : ?>
   				<?php echo wpautop('Sorry, No posts were found'); ?>
   			<?php endif; ?>
-
-        <!-- Comments section -->
-        <?php comments_template(); ?>
+				<br><br><br>
+				<!-- Comments section -->
+				<?php comments_template(); ?>
   		</div>
       <?php get_sidebar(); ?>
+
     </div>
 
-<?php get_footer(); ?>
+		<?php get_footer(); ?>
